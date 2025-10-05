f1edc6345f804358391b2aa2895cbb4a73d0f8aa
savagame <goodhabitsamsam@gmail.com>
2025-10-05 02:52:40 -0400

    chore: added 2 handlers

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ import cors;
 const app = express();   // create app
 app.use(cors())		 // use cors as a middleware
 
+app.get('/health', (req, res) => {
+	res.json({msg: "your health is ok"}, {status: 200});
+})
+
+app.post('/user/:id', (req,res) => {
+	res.json({msg: "your body is configured"}, {status: 200})
+})
